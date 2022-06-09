2e98d4ed7eef15731495d372a067ddf5e121c3c6
João Victor Bartolot <joao.bartolot@calindra.com.br>
2022-06-09 19:33:49 -0300

    First commit

---
+++ src/main/java/com/todo/TodoList/dto/ProjectDTO.java
@@ -0,0 +1,54 @@
+package com.todo.TodoList.dto;
+
+import com.todo.TodoList.model.TaskModel;
+
+import java.util.Date;
+import java.util.List;
+
+public class ProjectDTO {
+    public int id;
+    public String name;
+    public Date creationDate;
+    public Date updateDate;
+    public List<TaskModel> tasks;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public List<TaskModel> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<TaskModel> tasks) {
+        this.tasks = tasks;
+    }
+}
---
+++ src/main/java/com/todo/TodoList/model/TaskModel.java
@@ -0,0 +1,69 @@
+package com.todo.TodoList.model;
+
+import java.util.Date;
+
+public class TaskModel {
+    private int id;
+    private String title;
+    private String description;
+    private boolean done;
+    private Date creationDate;
+    private Date updateDate;
+    private String owner;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
