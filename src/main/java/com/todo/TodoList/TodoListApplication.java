50c3508ec0d48ce91a1561155f5ab7406ede7e94
João Bartolot <jvcbartolot@gmail.com>
2022-06-28 11:50:54 -0300

    feat: fixing deploy error

--- src/main/java/com/todo/TodoList/TodoListApplication.java
+++ src/main/java/com/todo/TodoList/TodoListApplication.java
@@ -1,7 +1,13 @@
 package com.todo.TodoList;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class TodoListApplication {
@@ -14,4 +20,20 @@ public class TodoListApplication {
 		}
 	}
 
+	@PostConstruct
+	public void initialize() {
+		try {
+
+			ClassPathResource serviceAccount =
+					new ClassPathResource("todo-list-bc3ad-firebase-adminsdk-zliph-2a7c1de0f2.json");
+
+			FirebaseOptions options = FirebaseOptions.builder()
+					.setCredentials(GoogleCredentials.fromStream(serviceAccount.getInputStream()))
+					.build();
+
+			FirebaseApp.initializeApp(options);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
